a6ced8e47c6471fa38a03b0872f614e5e25e5ad9
vu le <vusters@gmail.com>
2022-05-08 18:23:51 -0700

    update disabled color

--- lib/widget/form_helper.dart
+++ lib/widget/form_helper.dart
@@ -58,7 +58,7 @@ abstract class FormFieldWidgetState<W extends HasController> extends WidgetState
     if (widget.controller is FormFieldController) {
       FormFieldController myController = widget.controller as FormFieldController;
       return InputDecoration(
-          disabledBorder: const UnderlineInputBorder(borderSide: BorderSide(color: Color(0xffBDBDBD))),
+          disabledBorder: const UnderlineInputBorder(borderSide: BorderSide(color: Color(0xffE0E0E0))),
           contentPadding: const EdgeInsets.all(0),
           floatingLabelBehavior: FloatingLabelBehavior.always,
           labelText: shouldShowLabel() ? myController.label : null,
