fdd2658a5878f833c78eeac42f99d71c1fa8f246
vu le <vusters@gmail.com>
2022-05-25 11:17:10 -0700

    refine button styling

--- lib/widget/button.dart
+++ lib/widget/button.dart
@@ -1,6 +1,7 @@
 
 import 'package:ensemble/framework/action.dart' as ensemble;
 import 'package:ensemble/layout/form.dart';
+import 'package:ensemble/layout/layout_helper.dart';
 import 'package:ensemble/screen_controller.dart';
 import 'package:ensemble/util/utils.dart';
 import 'package:ensemble/framework/widget/widget.dart';
@@ -27,10 +28,8 @@ class Button extends StatefulWidget with Invokable, HasController<ButtonControll
 
       'enabled': (value) => _controller.enabled = Utils.optionalBool(value),
       'outline': (value) => _controller.outline = Utils.optionalBool(value),
-      'backgroundColor': (value) => _controller.backgroundColor = Utils.optionalInt(value),
-      'color': (value) => _controller.color = Utils.optionalInt(value),
-      'borderRadius': (value) => _controller.borderRadius = Utils.optionalInt(value),
-      'padding': (value) => _controller.padding = Utils.optionalInt(value),
+      'backgroundColor': (value) => _controller.backgroundColor = Utils.getColor(value),
+      'color': (value) => _controller.color = Utils.getColor(value),
     };
   }
   @override
@@ -43,64 +42,73 @@ class Button extends StatefulWidget with Invokable, HasController<ButtonControll
 
 }
 
-class ButtonController extends WidgetController {
+class ButtonController extends BoxController {
   late String label;
   ensemble.EnsembleAction? onTap;
 
   bool? enabled;
   bool? outline;
-  int? backgroundColor;
-  int? color;
-  int? borderRadius;
-  int? padding;
+  Color? backgroundColor;
+  Color? color;
 }
 
 
 class ButtonState extends WidgetState<Button> {
   @override
   Widget build(BuildContext context) {
+    // keep it simple, if border color is not set, use the backgroundColor.
+    // For outline button it makes sense, for solid button it doesn't matter.
+    Color borderColor =
+        widget._controller.borderColor ??
+        widget._controller.backgroundColor ??
+        Theme.of(context).colorScheme.primary;
 
     ButtonStyle buttonStyle = ButtonStyle(
       padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-        widget._controller.padding != null ?
-        EdgeInsets.all((widget._controller.padding!).toDouble()) :
-        const EdgeInsets.only(left: 15, top: 3, right: 15, bottom: 3)),
+          Utils.getInsets(
+              widget._controller.padding,
+              fallback: const EdgeInsets.only(left: 15, top: 3, right: 15, bottom: 3))),
       foregroundColor:
-        widget._controller.color is int ?
-        MaterialStateProperty.all<Color>(Color(widget._controller.color as int)) :
+        widget._controller.color != null ?
+        MaterialStateProperty.all<Color>(widget._controller.color!) :
         null,
       backgroundColor:
-        (widget._controller.outline is bool && widget._controller.outline as bool) || widget._controller.backgroundColor is! int ?
+        (widget._controller.outline is bool && widget._controller.outline as bool) || widget._controller.backgroundColor == null ?
         null :
-        MaterialStateProperty.all<Color>(Color(widget._controller.backgroundColor as int)),
+        MaterialStateProperty.all<Color>(widget._controller.backgroundColor!),
       shape: MaterialStateProperty.all<RoundedRectangleBorder>(
         RoundedRectangleBorder(
           borderRadius:
             widget._controller.borderRadius is int ?
             BorderRadius.circular((widget._controller.borderRadius as int).toDouble()) :
             BorderRadius.zero,
-          side: !isEnabled() ? BorderSide.none : BorderSide(color:
-              widget._controller.backgroundColor is int ?
-              Color(widget._controller.backgroundColor as int) :
-              Theme.of(context).colorScheme.primary)
+          side: !isEnabled() ? BorderSide.none : BorderSide(
+              color: borderColor,
+              width: (widget._controller.borderWidth ?? 1).toDouble())
         )
       )
     );
 
     Text label = Text(Utils.translate(widget._controller.label, context));
 
+    Widget? rtn;
     if (widget._controller.outline is bool && widget._controller.outline as bool) {
-      return TextButton(
+      rtn = TextButton(
         onPressed: isEnabled() ? () => onPressed(context) : null,
         style: buttonStyle,
         child: label);
     } else {
-      return ElevatedButton(
+      rtn = ElevatedButton(
         onPressed: isEnabled() ? () => onPressed(context) : null,
         style: buttonStyle,
         child: label);
     }
 
+    // add margin if specified
+    EdgeInsets? margin = Utils.optionalInsets(widget._controller.margin);
+    return margin != null ?
+      Padding(padding: margin, child: rtn) :
+        rtn;
   }
 
   void onPressed(BuildContext context) {
