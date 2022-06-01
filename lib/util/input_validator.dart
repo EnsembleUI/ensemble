09c59c6f545a5b2a490da5f88e21e84cba0ccc1e
vu le <vusters@gmail.com>
2022-05-31 18:30:30 -0700

    add validators for IP, Email, Phone

---
+++ lib/util/input_validator.dart
@@ -0,0 +1,18 @@
+import 'package:form_validator/form_validator.dart';
+
+class InputValidator {
+
+  /// check if value is a valid IPv4 or IPv6
+  static bool ipAddress(String value) {
+    String? error = ValidationBuilder().or(
+      (builder) => builder.ip(),
+      (builder) => builder.ipv6()).test(value);
+    return error == null;
+  }
+
+  /// check if value is a valid phone number
+  static bool phone(String value) {
+    return null == ValidationBuilder().phone().test(value);
+  }
+
+}
