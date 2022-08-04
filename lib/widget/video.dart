fb851285bca28873ce56fa06444a2f6f9e808f91
vu le <vu@ensembleui.com>
2022-08-04 11:40:15 -0700

    update Video widget

--- lib/widget/video.dart
+++ lib/widget/video.dart
@@ -16,13 +16,13 @@ import 'package:intl/intl.dart';
 import 'package:ensemble/util/extensions.dart';
 import 'package:video_player/video_player.dart';
 
-class Video extends StatefulWidget with Invokable, HasController<VideoController, VideoState> {
+class Video extends StatefulWidget with Invokable, HasController<MyController, VideoState> {
   static const type = 'Video';
   Video({Key? key}) : super(key: key);
 
-  final VideoController _controller = VideoController();
+  final MyController _controller = MyController();
   @override
-  VideoController get controller => _controller;
+  MyController get controller => _controller;
 
   @override
   State<StatefulWidget> createState() => VideoState();
@@ -49,9 +49,8 @@ class Video extends StatefulWidget with Invokable, HasController<VideoController
 
 }
 
-class VideoController extends WidgetController {
+class MyController extends WidgetController {
   VideoPlayerController? _playerController;
-  Future<void>? _playerControllerFuture;
 
   String? _source;
   void updateSource(String? value) {
@@ -59,8 +58,21 @@ class VideoController extends WidgetController {
     _playerController?.dispose();
 
     if (value != null) {
-      _playerController = VideoPlayerController.network(value);
-      _playerControllerFuture = _playerController!.initialize();
+      _playerController = VideoPlayerController.network(value)
+        ..initialize().then((_) {
+          VideoPlayerValue value = _playerController!.value;
+          log(value.toString());
+          notifyListeners();
+        });
+
+      _playerController!.addListener(() {
+        // finish playing, call setState() to update the status
+        if (_playerController!.value.position ==
+            _playerController!.value.duration) {
+          notifyListeners();
+        }
+      });
+
     }
   }
 
@@ -70,56 +82,45 @@ class VideoState extends WidgetState<Video> {
 
   @override
   Widget build(BuildContext context) {
-    if (widget._controller._playerController == null || widget._controller._playerControllerFuture == null) {
-      return Image.asset("assets/images/img_placeholder.png");
+    if (widget._controller._playerController == null || !widget._controller._playerController!.value.isInitialized) {
+      //return Image.asset("assets/images/img_placeholder.png");
+      return const SizedBox.shrink();
     }
 
-    return FutureBuilder(
-      future: widget._controller._playerControllerFuture,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          VideoPlayerController playerController = widget._controller._playerController!;
-          return AspectRatio(
-              aspectRatio: playerController.value.aspectRatio,
-              child: Stack(
-                alignment: Alignment.bottomCenter,
-                children: [
-                  VideoPlayer(playerController),
-                  Center(
-                    child: CircleAvatar(
-                      backgroundColor: Colors.white.withOpacity(.5),
-                      radius: 17,
-                      child: IconButton(
-                        padding: EdgeInsets.zero,
-                        icon: Icon(getVideoIconStatus(playerController)),
-                        color: Colors.black54,
-                        onPressed: () {
-                          setState(() {
-                            playerController.value.isPlaying ? playerController.pause() : playerController.play();
-                          });
-                        }
-                      )
-                    )
-                  )
-                ],
+    VideoPlayerController playerController = widget._controller._playerController!;
+    return AspectRatio(
+      aspectRatio: playerController.value.aspectRatio,
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          VideoPlayer(playerController),
+          Center(
+            child: CircleAvatar(
+              backgroundColor: Colors.white.withOpacity(.5),
+              radius: 17,
+              child: IconButton(
+                padding: EdgeInsets.zero,
+                icon: Icon(getVideoIconStatus(playerController)),
+                color: Colors.black54,
+                onPressed: () {
+                  setState(() {
+                    playerController.value.isPlaying ? playerController.pause() : playerController.play();
+                  });
+                }
               )
-          );
-
-
-        } else {
-           return const Center(
-             child: CircularProgressIndicator(),
-           );
-        }
-    });
+            )
+          )
+        ]
+      )
+    );
 
   }
 
   IconData getVideoIconStatus(VideoPlayerController playerController) {
     if (playerController.value.isPlaying) {
       return Icons.pause;
-    } else if (playerController.value.duration == playerController.value.position) {
-      return Icons.repeat;
+    } else if (playerController.value.duration > Duration.zero && playerController.value.duration == playerController.value.position) {
+      return Icons.restart_alt;
     }
     return Icons.play_arrow;
   }
