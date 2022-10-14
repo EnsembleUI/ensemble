1693d6c7fb569b61ebe322e602a4e07c454c205b
kmahmood74 <kmahmood74@users.noreply.github.com>
2022-10-14 15:10:22 -0700

    ES-226 support for iframes

---
+++ lib/widget/webview/native/webviewstate.dart
@@ -0,0 +1,82 @@
+import 'package:ensemble/framework/widget/widget.dart';
+import 'package:ensemble/widget/webview.dart';
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class ControllerImpl extends ViewController {
+  WebViewController? controller;
+  ControllerImpl([this.controller]);
+  @override
+  void loadUrl(String url) {
+    controller!.loadUrl(url);
+  }
+}
+class WebViewState extends WidgetState<EnsembleWebView> {
+  double? height = 0;
+  late ControllerImpl _controller;
+
+  @override
+  void initState() {
+    _controller = ControllerImpl();
+    super.initState();
+  }
+
+  @override
+  Widget buildWidget(BuildContext context) {
+    // WebView's height will be the same as the HTML height
+    Widget webView = SizedBox(
+        height: height,
+        child: WebView(
+            initialUrl: widget.controller.uri,
+            javascriptMode: JavascriptMode.unrestricted,
+            onWebViewCreated:  (controller) {
+              _controller.controller = controller;
+              widget.controller.webViewController = _controller;
+            },
+            onWebResourceError: (WebResourceError err) {
+              setState(() {
+                widget.controller.error = "Error loading html content";
+              });
+            },
+            onProgress: (value) {
+              setState(() {
+                widget.controller.loadingPercent = value;
+              });
+            },
+            onPageFinished: (param) async {
+              height = double.parse(
+                  await _controller.controller!.runJavascriptReturningResult(
+                      "document.documentElement.scrollHeight;"));
+              setState(() {
+                widget.controller.loadingPercent = 100;
+              });
+
+            })
+    );
+
+    return Stack(
+      alignment: Alignment.topLeft,
+      children: [
+        webView,
+        // loading indicator
+        Visibility(
+            visible: widget.controller.loadingPercent! > 0 && widget.controller.loadingPercent! < 100 && widget.controller.error == null,
+            child: LinearProgressIndicator(
+                minHeight: 3,
+                value: widget.controller.loadingPercent! / 100.0
+            )
+        ),
+        // error panel
+        Visibility(
+          visible: widget.controller.error != null,
+          child: Center(
+              child: Text(widget.controller.error ?? '')
+          ),
+        ),
+
+      ],
+    );
+
+  }
+
+}
---
+++ lib/widget/webview/webviewstate.dart
@@ -0,0 +1,5 @@
+library webviewstate;
+
+export 'native/unsupportedwebview.dart'
+if (dart.library.html) 'web/webviewstate.dart'
+if (dart.library.io) 'native/webviewstate.dart';
