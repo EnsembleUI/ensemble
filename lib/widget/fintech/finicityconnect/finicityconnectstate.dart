c370c600243afa3c5feb832b60211a7dd89bb972
kmahmood74 <kmahmood74@users.noreply.github.com>
2023-01-31 11:38:17 -0800

    https://github.com/EnsembleUI/ensemble/issues/53 support for finicity connect on webview - tested with iOS simulator, needs testing on android. Also refactored code.

---
+++ lib/widget/fintech/finicityconnect/finicityconnectstate.dart
@@ -0,0 +1,5 @@
+library finicityconnectstate;
+
+export 'native/unsupportedfinicityconnectstate.dart'
+if (dart.library.html) 'web/finicityconnectstate.dart'
+if (dart.library.io) 'native/finicityconnectstate.dart';
