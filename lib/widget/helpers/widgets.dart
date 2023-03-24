75a336686a8d52fb59b0aba43bcca158166a340a
vu le <vu@ensembleui.com>
2023-03-23 17:31:17 -0700

    Certain containers (e.g. DataGrid) requires its children to report their intrinsic heights for proper rendering. This cause issues as our Input widgets use LayoutBuilder which doesn't support it. This will fix it by not using LayoutBuilder if we have parents like that.

--- lib/widget/helpers/widgets.dart
+++ lib/widget/helpers/widgets.dart
@@ -1,6 +1,7 @@
 /// This class contains common widgets for use with Ensemble widgets.
 
 import 'package:ensemble/framework/error_handling.dart';
+import 'package:ensemble/framework/widget/widget.dart';
 import 'package:ensemble/widget/input/form_helper.dart';
 import 'package:ensemble/widget/helpers/controllers.dart';
 import 'package:ensemble/framework/theme/theme_manager.dart';
@@ -24,6 +25,7 @@ class BoxWrapper extends StatelessWidget {
 
       // width/height maybe applied at the child, or not applicable
       this.ignoresDimension = false});
+
   final Widget widget;
   final BoxController boxController;
 
@@ -98,30 +100,47 @@ class BoxWrapper extends StatelessWidget {
 /// the case where it is put inside a Row without expanded flag.
 class InputWrapper extends StatelessWidget {
   const InputWrapper(
-      {super.key, required this.type, required this.widget, required this.controller});
+      {super.key,
+      required this.type,
+      required this.widget,
+      required this.controller});
+
   final String type;
   final Widget widget;
   final FormFieldController controller;
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(builder: (context, constraints) {
+    Widget rtn = controller.maxWidth == null
+        ? widget
+        : ConstrainedBox(
+            constraints:
+                BoxConstraints(maxWidth: controller.maxWidth!.toDouble()),
+            child: widget);
 
-      // inside a e.g. Row but not wrapping inside Expanded.
-      // This is the error condition we need to advise the user
-      if (!constraints.hasBoundedWidth && !controller.expanded) {
-        // throw Error when input widgets (which stretch to their parent) are
-        // inside a Row (which allow its child to have as much space as it wants)
-        // without using expanded flag.
-        throw LanguageError("${type} widget requires a width when used inside a parent like Row.", recovery: "Consider using 'expanded: true' on the ${type} to fill the parent's available width.");
-      }
-
-      return controller.maxWidth == null
-          ? widget
-          : ConstrainedBox(
-              constraints: BoxConstraints(maxWidth: controller.maxWidth!.toDouble()),
-              child: widget);
-    });
+    // we'd like to use LayoutBuilder to detect layout anomaly, but certain
+    // containers don't like LayoutBuilder, since it doesn't support returning
+    // intrinsic Width/Height
+    RequiresChildWithIntrinsicDimension? requiresChildWithIntrinsicDimension =
+        context.dependOnInheritedWidgetOfExactType<
+            RequiresChildWithIntrinsicDimension>();
+    if (requiresChildWithIntrinsicDimension == null) {
+      return LayoutBuilder(builder: (context, constraints) {
+        // inside a e.g. Row but not wrapping inside Expanded.
+        // This is the error condition we need to advise the user
+        if (!constraints.hasBoundedWidth && !controller.expanded) {
+          // throw Error when input widgets (which stretch to their parent) are
+          // inside a Row (which allow its child to have as much space as it wants)
+          // without using expanded flag.
+          throw LanguageError(
+              "${type} widget requires a width when used inside a parent like Row.",
+              recovery:
+                  "Consider using 'expanded: true' on the ${type} to fill the parent's available width.");
+        }
+        return rtn;
+      });
+    }
+    return rtn;
   }
 }
 
@@ -132,6 +151,7 @@ class InputWrapper extends StatelessWidget {
 class ClearableInput extends StatelessWidget {
   const ClearableInput(
       {super.key, required this.text, required this.onCleared, this.textStyle});
+
   final String text;
   final TextStyle? textStyle;
   final dynamic onCleared;
