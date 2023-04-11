5bcef6368194e140d6ecb640a509273eeaa47479
Sneh Mehta <snehmehta933@gmail.com>
2023-04-11 13:44:21 +0530

    added pickFiles to action parser

--- lib/framework/action.dart
+++ lib/framework/action.dart
@@ -339,14 +339,18 @@ class FilePickerAction extends EnsembleAction {
   EnsembleAction? onError;
 
   factory FilePickerAction.fromYaml({YamlMap? payload}) {
+    if (payload == null || payload['id'] == null) {
+      throw LanguageError("${ActionType.pickFiles.name} requires 'id'.");
+    }
+
     return FilePickerAction(
-      id: payload?['id'],
+      id: Utils.getString(payload['id'], fallback: ''),
       allowedExtensions:
-          (payload?['allowedExtensions'] as YamlList?)?.cast<String>().toList(),
-      allowMultiple: Utils.optionalBool(payload?['allowMultiple']),
-      allowCompression: Utils.optionalBool(payload?['allowCompression']),
-      onComplete: EnsembleAction.fromYaml(payload?['onComplete']),
-      onError: EnsembleAction.fromYaml(payload?['onError']),
+          (payload['allowedExtensions'] as YamlList?)?.cast<String>().toList(),
+      allowMultiple: Utils.optionalBool(payload['allowMultiple']),
+      allowCompression: Utils.optionalBool(payload['allowCompression']),
+      onComplete: EnsembleAction.fromYaml(payload['onComplete']),
+      onError: EnsembleAction.fromYaml(payload['onError']),
     );
   }
 }
@@ -539,6 +543,8 @@ abstract class EnsembleAction {
               min: 50));
     } else if (actionType == ActionType.uploadFiles) {
       return FileUploadAction.fromYaml(payload: payload);
+    } else if (actionType == ActionType.pickFiles) {
+      return FilePickerAction.fromYaml(payload: payload);
     } else if (actionType == ActionType.openUrl) {
       return OpenUrlAction.fromYaml(payload: payload);
     } else if (actionType == ActionType.connectWallet) {
