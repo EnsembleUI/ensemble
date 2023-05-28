a493460efdca40036ec5bd222a0eac5c78551606
Vu Le <vu@ensembleui.com>
2023-05-27 20:08:29 -0700

    add Remix icons

---
+++ lib/util/fonts/remix.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+class Remix {
+
+
+  static const IconData image_2_line =
+    IconData(0xEE45, fontFamily: 'Remix', fontPackage: 'ensemble');
+  static const IconData time_line =
+    IconData(0xF20F, fontFamily: 'Remix', fontPackage: 'ensemble');
+  static const IconData thermometer_line =
+    IconData(0xF203, fontFamily: 'Remix', fontPackage: 'ensemble');
+  static const IconData first_aid_kit_line =
+    IconData(0xED37, fontFamily: 'Remix', fontPackage: 'ensemble');
+  static const IconData shield_check_line =
+    IconData(0xF100, fontFamily: 'Remix', fontPackage: 'ensemble');
+}
