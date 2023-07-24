9b5a3d662a5d46d856c257eb85490cec85fdce90
Vu Le <vu@ensembleui.com>
2023-07-24 14:22:03 -0700

    delete moved files

--- lib/framework/placeholder/auth_context_manager.dart
+++
@@ -1,9 +0,0 @@
-import 'package:ensemble/framework/error_handling.dart';
-
-abstract class AuthContextManagerBase {}
-
-class AuthContextManagerStub implements AuthContextManagerBase {
-  AuthContextManagerStub() {
-    throw RuntimeError('Auth is not enabled.');
-  }
-}
