6244ab480021d7a432ecdec3177479b5e73d1099
Sneh Mehta <snehmehta933@gmail.com>
2023-07-24 16:17:10 +0530

    added web notification

--- lib/ensemble.dart
+++ lib/ensemble.dart
@@ -64,11 +64,7 @@ class Ensemble {
       // These are not secrets so OK to include here.
       // https://firebase.google.com/docs/projects/api-keys#api-keys-for-firebase-are-different
       ensembleFirebaseApp = await Firebase.initializeApp(
-          name: kIsWeb
-              ? null
-              : Firebase.apps.isNotEmpty
-                  ? 'ensemble'
-                  : null,
+          name: Firebase.apps.isNotEmpty ? 'ensemble' : null,
           options: const FirebaseOptions(
               apiKey: 'AIzaSyBAZ7wf436RSbcXvhhfg7e4TUh6A2SKve8',
               appId: '1:326748243798:ios:30f2a4f824dc58ea94b8f7',
