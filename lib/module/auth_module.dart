0fa73d29f2131760914cebbd1637e6e73a868635
Vu Le <vu@ensembleui.com>
2023-07-24 14:21:50 -0700

    make Module a bit more user friendly

--- lib/module/auth_module.dart
+++ lib/module/auth_module.dart
@@ -1,21 +1,21 @@
-import 'package:ensemble/framework/placeholder/auth_context_manager.dart';
-import 'package:ensemble/framework/placeholder/oauth_controller.dart';
-import 'package:ensemble/framework/placeholder/token_manager.dart';
+import 'package:ensemble/framework/stub/auth_context_manager.dart';
+import 'package:ensemble/framework/stub/oauth_controller.dart';
+import 'package:ensemble/framework/stub/token_manager.dart';
 import 'package:ensemble/widget/stub_widgets.dart';
 import 'package:get_it/get_it.dart';
 
-abstract class AuthModule {
-  void init();
-}
+abstract class AuthModule {}
 
 class AuthModuleStub implements AuthModule {
   @override
   void init() {
-    GetIt.I.registerFactory<AuthContextManagerBase>(() => AuthContextManagerStub());
-    GetIt.I.registerFactory<SignInWithGoogleBase>(() => const SignInWithGoogleStub());
-    GetIt.I.registerFactory<SignInWithAppleBase>(() => const SignInWithAppleStub());
-    GetIt.I.registerSingleton<TokenManagerBase>(TokenManagerStub());
-    GetIt.I.registerFactory<OAuthControllerBase>(() => OAuthControllerStub());
+    GetIt.I.registerFactory<SignInWithGoogle>(() => const SignInWithGoogleStub());
+    GetIt.I.registerFactory<SignInWithApple>(() => const SignInWithAppleStub());
+    GetIt.I.registerSingleton<TokenManager>(TokenManagerStub());
+    GetIt.I.registerFactory<OAuthController>(() => OAuthControllerStub());
+
+    // note that we don't inject AuthContextManagerStub(), since its presence
+    // will prevent data_context to load
   }
 
 }
