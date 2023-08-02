874e1549d6ea61697468644ab4c240d61838a57b
Vu Le <vu@ensembleui.com>
2023-08-01 22:24:04 -0700

    initialize secrets on app start up

--- lib/framework/secrets.dart
+++ lib/framework/secrets.dart
@@ -7,7 +7,33 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import '../ensemble.dart';
 
 class SecretsStore with Invokable {
-  final Map<String, dynamic> secretCache = {};
+  static final SecretsStore _instance = SecretsStore._internal();
+  SecretsStore._internal();
+  factory SecretsStore() {
+    return _instance;
+  }
+  bool _initialized = false;
+  final Map<String, String> secretCache = {};
+
+  /// read all the secrets from server and local overrides
+  Future<void> initialize() async {
+    if (!_initialized) {
+      // add from remote
+      var secrets = Ensemble().getConfig()?.definitionProvider.getSecrets() ?? {};
+      
+      // add local overrides
+      try {
+        await dotenv.load();
+        secrets.addAll(dotenv.env);
+      } catch (_) {}
+
+      for (var entry in secrets.entries) {
+        _cacheSecret(entry.key, entry.value);
+      }
+
+      _initialized = true;
+    }
+  }
 
   @override
   Map<String, Function> getters() {
