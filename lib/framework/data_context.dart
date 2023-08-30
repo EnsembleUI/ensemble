e832021baa36871f23d4718c78ca0b8b044ebe78
Vu <vusters@gmail.com>
2023-08-29 20:37:48 -0700

    Storage set/delete now properly dispatch changes

--- lib/framework/data_context.dart
+++ lib/framework/data_context.dart
@@ -469,10 +469,11 @@ class EnsembleStorage with Invokable {
   Map<String, Function> methods() {
     return {
       'get': (String key) => StorageManager().read(key),
-      'set': (String key, dynamic value) => value == null
-          ? StorageManager().remove(key)
-          : StorageManager().write(key, value),
-      'delete': (key) => StorageManager().remove(key)
+      'set': setProperty,
+      'delete': (key) {
+        StorageManager().remove(key);
+        ScreenController().dispatchStorageChanges(context, key, null);
+      }
     };
   }
 
