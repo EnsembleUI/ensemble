8729af0bdb2260f4c247a0ad6f79b1f03e239acd
vinothvino42 <vinothvino42@gmail.com>
2023-08-31 21:35:55 +0530

    fix: bottom nav bar item issue

--- lib/framework/view/bottom_nav_page_group.dart
+++ lib/framework/view/bottom_nav_page_group.dart
@@ -110,11 +110,13 @@ class _BottomNavPageGroupState extends State<BottomNavPageGroup> {
         child: customIcon ??
             FloatingActionButton(
               backgroundColor: floatingBackgroundColor,
-              child: ensemble.Icon(
-                fabMenuItem!.icon ?? '',
-                library: fabMenuItem!.iconLibrary,
-                color: floatingItemColor,
-              ),
+              child: (fabMenuItem!.icon != null
+                  ? ensemble.Icon.fromModel(
+                      fabMenuItem!.icon!,
+                      fallbackLibrary: fabMenuItem!.iconLibrary,
+                      fallbackColor: floatingItemColor,
+                    )
+                  : ensemble.Icon('')),
               onPressed: () => _floatingButtonTapped(fabMenuItem!),
             ),
       );
@@ -170,21 +172,17 @@ class _BottomNavPageGroupState extends State<BottomNavPageGroup> {
 
       final icon = customIcon ??
           (item.icon != null
-            ? ensemble.Icon.fromModel(
-                item.icon!,
-                fallbackLibrary: item.iconLibrary,
-                fallbackColor: unselectedColor)
-            : ensemble.Icon(''));
+              ? ensemble.Icon.fromModel(item.icon!,
+                  fallbackLibrary: item.iconLibrary,
+                  fallbackColor: unselectedColor)
+              : ensemble.Icon(''));
 
       final activeIcon = customActiveIcon ??
           (item.activeIcon != null || item.icon != null
-            ? ensemble.Icon.fromModel(
-                (item.activeIcon ?? item.icon)!,
-                fallbackColor: selectedColor,
-                fallbackLibrary: item.iconLibrary)
-            : null);
-
-
+              ? ensemble.Icon.fromModel((item.activeIcon ?? item.icon)!,
+                  fallbackColor: selectedColor,
+                  fallbackLibrary: item.iconLibrary)
+              : null);
 
       navItems.add(
         FABBottomAppBarItem(
