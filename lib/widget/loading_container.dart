6f7a221e1614d11ecc26b16d3f418ffc003f538c
vinothvino42 <vinothvino42@gmail.com>
2023-09-07 17:38:10 +0530

    feat: add shimmer support for image and refactor the shimmer

--- lib/widget/loading_container.dart
+++ lib/widget/loading_container.dart
@@ -7,7 +7,6 @@ import 'package:ensemble/widget/helpers/widgets.dart';
 import 'package:ensemble/widget/shape.dart';
 import 'package:ensemble_ts_interpreter/invokables/invokable.dart';
 import 'package:flutter/material.dart';
-import 'package:shimmer/shimmer.dart';
 
 import '../framework/view/page.dart';
 
@@ -92,12 +91,16 @@ class LoadingContainerState extends WidgetState<LoadingContainer> {
             opacity: 1,
             duration: const Duration(milliseconds: 300),
             child: widget._controller.useShimmer == true
-                ? Shimmer.fromColors(
-                    baseColor: const Color(0xFFE0E0E0),
-                    highlightColor: const Color(0xFFF5F5F7).withOpacity(0.5),
-                    child: loadingWidget ??
-                        DefaultLoadingShape(
-                            padding: widget._controller.defaultShimmerPadding))
+                ? CustomShimmer(
+                    linearGradient: _shimmerGradient,
+                    child: ShimmerLoading(
+                      isLoading: true,
+                      child: loadingWidget ??
+                          DefaultLoadingShape(
+                              padding:
+                                  widget._controller.defaultShimmerPadding),
+                    ),
+                  )
                 : loadingWidget ?? const SizedBox.shrink()),
 
       // fade in main content
@@ -166,4 +169,178 @@ class ListDetailShape extends StatelessWidget {
           )
         ],
       );
+}
+
+const _shimmerGradient = LinearGradient(
+  colors: [
+    Color(0xFFEBEBF4),
+    Color(0xFFF4F4F4),
+    Color(0xFFEBEBF4),
+  ],
+  stops: [
+    0.1,
+    0.3,
+    0.4,
+  ],
+  begin: Alignment(-1.0, -0.3),
+  end: Alignment(1.0, 0.3),
+  tileMode: TileMode.clamp,
+);
+
+class _SlidingGradientTransform extends GradientTransform {
+  const _SlidingGradientTransform({
+    required this.slidePercent,
+  });
+
+  final double slidePercent;
+
+  @override
+  Matrix4? transform(Rect bounds, {TextDirection? textDirection}) {
+    return Matrix4.translationValues(bounds.width * slidePercent, 0.0, 0.0);
+  }
+}
+
+class CustomShimmer extends StatefulWidget {
+  static CustomShimmerState? of(BuildContext context) {
+    return context.findAncestorStateOfType<CustomShimmerState>();
+  }
+
+  const CustomShimmer({
+    super.key,
+    required this.linearGradient,
+    this.child,
+  });
+
+  final LinearGradient linearGradient;
+  final Widget? child;
+
+  @override
+  CustomShimmerState createState() => CustomShimmerState();
+}
+
+class CustomShimmerState extends State<CustomShimmer>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _shimmerController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _shimmerController = AnimationController.unbounded(vsync: this)
+      ..repeat(min: -0.5, max: 1.5, period: const Duration(milliseconds: 1000));
+  }
+
+  @override
+  void dispose() {
+    _shimmerController.dispose();
+    super.dispose();
+  }
+
+  LinearGradient get gradient => LinearGradient(
+        colors: widget.linearGradient.colors,
+        stops: widget.linearGradient.stops,
+        begin: widget.linearGradient.begin,
+        end: widget.linearGradient.end,
+        transform:
+            _SlidingGradientTransform(slidePercent: _shimmerController.value),
+      );
+
+  bool get isSized =>
+      (context.findRenderObject() as RenderBox?)?.hasSize ?? false;
+
+  Size get size => (context.findRenderObject() as RenderBox).size;
+
+  Offset getDescendantOffset({
+    required RenderBox descendant,
+    Offset offset = Offset.zero,
+  }) {
+    final shimmerBox = context.findRenderObject() as RenderBox;
+    return descendant.localToGlobal(offset, ancestor: shimmerBox);
+  }
+
+  Listenable get shimmerChanges => _shimmerController;
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.child ?? const SizedBox();
+  }
+}
+
+class ShimmerLoading extends StatefulWidget {
+  const ShimmerLoading({
+    super.key,
+    required this.isLoading,
+    required this.child,
+  });
+
+  final bool isLoading;
+  final Widget child;
+
+  @override
+  State<ShimmerLoading> createState() => _ShimmerLoadingState();
+}
+
+class _ShimmerLoadingState extends State<ShimmerLoading> {
+  Listenable? _shimmerChanges;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    if (_shimmerChanges != null) {
+      _shimmerChanges!.removeListener(_onShimmerChange);
+    }
+    _shimmerChanges = CustomShimmer.of(context)?.shimmerChanges;
+    if (_shimmerChanges != null) {
+      _shimmerChanges!.addListener(_onShimmerChange);
+    }
+  }
+
+  @override
+  void dispose() {
+    _shimmerChanges?.removeListener(_onShimmerChange);
+    super.dispose();
+  }
+
+  void _onShimmerChange() {
+    if (widget.isLoading) {
+      setState(() {
+        // update the shimmer painting.
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!widget.isLoading) {
+      return widget.child;
+    }
+
+    // Collect ancestor shimmer information.
+    final shimmer = CustomShimmer.of(context)!;
+    if (!shimmer.isSized) {
+      // The ancestor Shimmer widget isn’t laid
+      // out yet. Return an empty box.
+      return const SizedBox();
+    }
+    final shimmerSize = shimmer.size;
+    final gradient = shimmer.gradient;
+    final offsetWithinShimmer = shimmer.getDescendantOffset(
+      descendant: context.findRenderObject() as RenderBox,
+    );
+
+    return ShaderMask(
+      blendMode: BlendMode.srcATop,
+      shaderCallback: (bounds) {
+        return gradient.createShader(
+          Rect.fromLTWH(
+            -offsetWithinShimmer.dx,
+            -offsetWithinShimmer.dy,
+            shimmerSize.width,
+            shimmerSize.height,
+          ),
+        );
+      },
+      child: widget.child,
+    );
+  }
 }
