eef90e8e7d55da63811eb02c5ba746abb976c73a
vinothvino42 <vinothvino42@gmail.com>
2023-10-04 18:17:04 +0530

    feat: add deep link navigator with screen name as a base class

--- lib/ios_deep_link_manager.dart
+++ lib/ios_deep_link_manager.dart
@@ -1,13 +1,12 @@
-import 'package:ensemble/screen_controller.dart';
-import 'package:ensemble/util/utils.dart';
+import 'package:ensemble/deep_link_manager.dart';
 import 'package:flutter/services.dart';
 
-class IOSDeepLinkManager {
+class IOSDeepLinkManager extends DeepLinkNavigator {
   static final IOSDeepLinkManager _instance = IOSDeepLinkManager._internal();
   static const MethodChannel _platform = MethodChannel('com.tin.mobile');
 
   // This method is invoked from the Host(iOS) Platform AppDelegate.swift
-  static const String _deepLinkMethod = "urlOpened";
+  static const String _deepLinkMethod = 'urlOpened';
 
   IOSDeepLinkManager._internal();
 
@@ -20,21 +19,10 @@ class IOSDeepLinkManager {
       if (call.method == IOSDeepLinkManager._deepLinkMethod) {
         final url = Uri.parse(call.arguments);
         Future.delayed(const Duration(seconds: 6), () {
-          _navigateToScreen(url);
+          navigateToScreen(url);
         });
       }
       return Future.value(true);
     });
-  }
-
-  /// navigate to screen if the deep link specifies a screenId param
-  void _navigateToScreen(Uri uri) {
-    String? screenId =
-        (uri.queryParameters['screenId'] ?? uri.queryParameters['screenid'])
-            ?.toString();
-    if (screenId != null) {
-      ScreenController().navigateToScreen(Utils.globalAppKey.currentContext!,
-          screenId: screenId);
-    }
   }
 }
