c7af5ca73999b15b6fb297d916e480d9a7fe8357
vinothvino42 <vinothvino42@gmail.com>
2023-10-26 16:03:30 +0530

    refactor: change it to flutter secure storage package instead of using platform channel

---
+++ lib/framework/keychain_manager.dart
@@ -0,0 +1,34 @@
+import 'package:ensemble/framework/error_handling.dart';
+import 'package:ensemble/framework/storage_manager.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class KeychainManager {
+  static final KeychainManager _instance = KeychainManager._internal();
+
+  KeychainManager._internal();
+
+  factory KeychainManager() {
+    return _instance;
+  }
+
+  Future<void> saveToKeychain(String key, dynamic value,
+      {String? groupId}) async {
+    try {
+      final iOSOptions = groupId != null ? IOSOptions(groupId: groupId) : null;
+      await StorageManager().writeSecurely(
+          key: key, value: value.toString(), iosOptions: iOSOptions);
+    } catch (e) {
+      throw LanguageError('Failed to store value. Reason: ${e.toString()}');
+    }
+  }
+
+  Future<void> clearKeychain(String key, {String? groupId}) async {
+    try {
+      final iOSOptions = groupId != null ? IOSOptions(groupId: groupId) : null;
+      await StorageManager().remove(key, iosOptions: iOSOptions);
+    } catch (e) {
+      throw LanguageError(
+          'Failed to remove value with the key - $key. Reason: ${e.toString()}');
+    }
+  }
+}
