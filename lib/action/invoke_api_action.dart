8005bc6395d5c37f65825fe8f0685a23e3821270
Vu Le <vusters@gmail.com>
2023-10-26 19:10:58 -0700

    refactored Actions to have a single source of data context (ScopeManager)

--- lib/action/invoke_api_action.dart
+++ lib/action/invoke_api_action.dart
@@ -49,8 +49,7 @@ class InvokeAPIAction extends EnsembleAction {
   @override
   Future execute(BuildContext context, ScopeManager scopeManager,
       {DataContext? dataContext}) {
-    DataContext realDataContext = dataContext ?? scopeManager.dataContext;
-    var evalApiName = realDataContext.eval(apiName);
+    var evalApiName = scopeManager.dataContext.eval(apiName);
     var cloneAction = InvokeAPIAction(
         apiName: evalApiName,
         initiator: initiator,
@@ -58,8 +57,8 @@ class InvokeAPIAction extends EnsembleAction {
         inputs: inputs,
         onResponse: onResponse,
         onError: onError);
-    return InvokeAPIController().execute(cloneAction, context, realDataContext,
-        scopeManager, scopeManager.pageData.apiMap);
+    return InvokeAPIController().execute(
+        cloneAction, context, scopeManager, scopeManager.pageData.apiMap);
   }
 }
 
@@ -67,60 +66,61 @@ class InvokeAPIController {
   Future<Response?> executeWithContext(
       BuildContext context, InvokeAPIAction action,
       {Map<String, dynamic>? additionalInputs}) {
-    ScopeManager? scopeManager = ScreenController().getScopeManager(context);
-    if (scopeManager != null) {
+    ScopeManager? foundScopeManager =
+        ScreenController().getScopeManager(context);
+    if (foundScopeManager != null) {
+      // we need an ephemeral scope to append data to
+      ScopeManager scopeManager =
+          foundScopeManager.createChildScope(ephemeral: true);
+
       // add additional data if specified
       DataContext dataContext = scopeManager.dataContext;
       if (additionalInputs != null) {
         dataContext.addDataContext(additionalInputs);
       }
 
-      return execute(action, context, dataContext, scopeManager,
-          scopeManager.pageData.apiMap);
+      return execute(
+          action, context, scopeManager, scopeManager.pageData.apiMap);
     }
     throw Exception('Unable to execute API from context');
   }
 
-  Future<Response?> execute(
-      InvokeAPIAction action,
-      BuildContext context,
-      DataContext dataContext,
-      ScopeManager? scopeManager,
-      Map<String, YamlMap>? apiMap) async {
+  Future<Response?> execute(InvokeAPIAction action, BuildContext context,
+      ScopeManager scopeManager, Map<String, YamlMap>? apiMap) async {
     YamlMap? apiDefinition = apiMap?[action.apiName];
     if (apiDefinition != null) {
       // evaluate input arguments and add them to context
       if (apiDefinition['inputs'] is YamlList && action.inputs != null) {
         for (var input in apiDefinition['inputs']) {
-          dynamic value = dataContext.eval(action.inputs![input]);
+          dynamic value = scopeManager.dataContext.eval(action.inputs![input]);
           if (value != null) {
-            dataContext.addDataContextById(input, value);
+            scopeManager.dataContext.addDataContextById(input, value);
           }
         }
       }
 
       // if invokeAPI has an ID, add it to context so we can bind to it
       // This is useful when the API is called in a loop, so binding to its API name won't work properly
-      if (action.id != null && !dataContext.hasContext(action.id!)) {
-        scopeManager!.dataContext
-            .addInvokableContext(action.id!, APIResponse());
+      if (action.id != null &&
+          !scopeManager.dataContext.hasContext(action.id!)) {
+        scopeManager.dataContext.addInvokableContext(action.id!, APIResponse());
       }
 
+      dynamic errorResponse;
       try {
-        Response response =
-            await HttpUtils.invokeApi(context, apiDefinition, dataContext);
+        Response response = await HttpUtils.invokeApi(
+            context, apiDefinition, scopeManager.dataContext);
         if (response.isSuccess) {
-          _onAPIComplete(context, dataContext, action, apiDefinition, response,
-              apiMap, scopeManager);
-        } else {
-          processAPIError(context, dataContext, action, apiDefinition, response,
-              apiMap, scopeManager);
+          _onAPIComplete(
+              context, action, apiDefinition, response, apiMap, scopeManager);
+          return response;
         }
-        return response;
+        errorResponse = response;
       } catch (error) {
-        processAPIError(context, dataContext, action, apiDefinition, error,
-            apiMap, scopeManager);
+        errorResponse = error;
       }
+      _onAPIError(
+          context, action, apiDefinition, errorResponse, apiMap, scopeManager);
     } else {
       throw RuntimeError("Unable to find api definition for ${action.apiName}");
     }
@@ -129,19 +129,17 @@ class InvokeAPIController {
   /// e.g upon return of API result
   void _onAPIComplete(
       BuildContext context,
-      DataContext dataContext,
       InvokeAPIAction action,
       YamlMap apiDefinition,
       Response response,
       Map<String, YamlMap>? apiMap,
-      ScopeManager? scopeManager) {
+      ScopeManager scopeManager) {
     // first execute API's onResponse code block
     EnsembleAction? onResponse = EnsembleAction.fromYaml(
         apiDefinition['onResponse'],
         initiator: action.initiator);
     if (onResponse != null) {
-      processAPIResponse(
-          context, dataContext, onResponse, response, apiMap, scopeManager,
+      _processOnResponse(context, onResponse, response, apiMap, scopeManager,
           apiChangeHandler: dispatchAPIChanges,
           action: action,
           modifiableAPIResponse: true);
@@ -153,20 +151,19 @@ class InvokeAPIController {
 
     // if our Action has onResponse, invoke that next
     if (action.onResponse != null) {
-      processAPIResponse(context, dataContext, action.onResponse!, response,
-          apiMap, scopeManager);
+      _processOnResponse(
+          context, action.onResponse!, response, apiMap, scopeManager);
     }
   }
 
   /// Executing the onResponse action. Note that this can be
   /// the API's onResponse or a caller's onResponse (e.g. onPageLoad's onResponse)
-  void processAPIResponse(
+  void _processOnResponse(
       BuildContext context,
-      DataContext dataContext,
       EnsembleAction onResponseAction,
       Response response,
       Map<String, YamlMap>? apiMap,
-      ScopeManager? scopeManager,
+      ScopeManager apiScopeManager,
       {Function? apiChangeHandler,
       InvokeAPIAction? action,
       bool? modifiableAPIResponse}) {
@@ -175,14 +172,26 @@ class InvokeAPIController {
         ? ModifiableAPIResponse(response: response)
         : APIResponse(response: response);
 
-    // add the response to data context
-    DataContext localizedContext = dataContext.clone();
-    localizedContext.addInvokableContext('response', apiResponse);
-    localizedContext.addInvokableContext(
+    /// Here we received the ephemeral apiScopeManager. But we are now inside
+    /// the onResponse clauses which append more data specific to onResponse
+    /// only, so create a child ephemeral scope
+    ScopeManager scopeManager =
+        apiScopeManager.createChildScope(ephemeral: true);
+    scopeManager.dataContext.addInvokableContext('response', apiResponse);
+    scopeManager.dataContext.addInvokableContext(
         'event', EnsembleEvent(action?.initiator, data: apiResponse));
 
-    ScreenController().nowExecuteAction(
-        context, localizedContext, onResponseAction, apiMap, scopeManager);
+    // we are inside the API's onResponse, but by this time we have not
+    // dispatch changes to the API yet, since we allow the user to modify it.
+    // For this reason, add the apiName to this localized context so it
+    // can be accessed down the chain of this API's onResponse ONLY.
+    if (modifiableAPIResponse == true && action != null) {
+      scopeManager.dataContext.addInvokableContext(action.apiName, apiResponse);
+    }
+
+    // we are not waiting for nested Actions to complete
+    ScreenController()
+        .nowExecuteAction(context, onResponseAction, apiMap, scopeManager);
 
     if (modifiableAPIResponse == true) {
       // should be on Action's callback instead
@@ -191,29 +200,29 @@ class InvokeAPIController {
   }
 
   /// executing the onError action
-  void processAPIError(
+  void _onAPIError(
       BuildContext context,
-      DataContext dataContext,
       InvokeAPIAction action,
       YamlMap apiDefinition,
       dynamic errorResponse,
       Map<String, YamlMap>? apiMap,
-      ScopeManager? scopeManager) {
-    //log("Error: $error");
-
-    DataContext localizedContext = dataContext.clone();
+      ScopeManager apiScopeManager) {
+    /// Create child scope applicable for onError only
+    ScopeManager scopeManager =
+        apiScopeManager.createChildScope(ephemeral: true);
 
     String? errorStr;
     dynamic data;
     if (errorResponse is Response) {
       APIResponse apiResponse = APIResponse(response: errorResponse);
-      localizedContext.addInvokableContext('response', apiResponse);
+      scopeManager.dataContext.addInvokableContext('response', apiResponse);
       errorStr = errorResponse.reasonPhrase;
       data = apiResponse;
 
-      // dispatch the changes to the response
+      /// dispatch changes.Note that here we send update with the original
+      /// scopeManager, NOT the ephemeral onError one.
       dispatchAPIChanges(
-          scopeManager, action, APIResponse(response: errorResponse));
+          apiScopeManager, action, APIResponse(response: errorResponse));
     } else {
       errorStr = errorResponse is Error ? errorResponse.toString() : null;
     }
@@ -224,13 +233,15 @@ class InvokeAPIController {
         EnsembleAction.fromYaml(apiDefinition['onError']);
     if (onErrorAction != null) {
       ScreenController().nowExecuteAction(
-          context, localizedContext, onErrorAction, apiMap, scopeManager, event: apiEvent);
+          context, onErrorAction, apiMap, scopeManager,
+          event: apiEvent);
     }
 
     // if our Action has onError, invoke that next
     if (action.onError != null) {
       ScreenController().nowExecuteAction(
-          context, localizedContext, action.onError!, apiMap, scopeManager, event: apiEvent);
+          context, action.onError!, apiMap, scopeManager,
+          event: apiEvent);
     }
 
     // silently fail if error handle is not defined? or should we alert user?
