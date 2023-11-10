2e724bf8e33196532af46bfd4214cab9fe858870
Hemish Pancholi <hemishpancholi11@gmail.com>
2023-11-10 23:48:10 +0530

    Added PR Changes

--- lib/action/haptic_action.dart
+++ lib/action/haptic_action.dart
@@ -1,6 +1,7 @@
 import 'package:ensemble/framework/action.dart';
 import 'package:ensemble/framework/error_handling.dart';
 import 'package:ensemble/framework/scope.dart';
+import 'package:ensemble/screen_controller.dart';
 import 'package:ensemble/util/utils.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
@@ -14,20 +15,10 @@ enum HapticType {
 }
 
 class HapticAction extends EnsembleAction {
-  HapticAction(this.type);
+  HapticAction({required this.type, required this.onComplete});
 
-  final HapticType type;
-
-  // factory HapticAction.fromMap(dynamic inputs) =>
-  //     HapticAction.fromYaml(payload: Utils.getYamlMap(inputs));
-
-  // factory HapticAction.fromYaml({Map? payload}) {
-  //   if (payload == null || payload['type'] == null) {
-  //     throw LanguageError("${ActionType.invokeHaptic.name} requires 'type'");
-  //   }
-
-  //   return HapticAction(hapticType);
-  // }
+  final String type;
+  final EnsembleAction? onComplete;
 
   factory HapticAction.from(dynamic inputs) {
     Map? payload;
@@ -39,26 +30,43 @@ class HapticAction extends EnsembleAction {
       throw LanguageError("${ActionType.invokeHaptic.name} requires 'type'");
     }
 
-    // payload['type'] = ScopeManager.eval(payload['type']); // TODO: Continue from here
-
-    final hapticType = HapticType //
-        .values
-        .firstWhere(
-      (element) => element.name == payload!['type'],
-      orElse: () => HapticType.heavyImpact,
+    return HapticAction(
+      type: payload['type'],
+      onComplete: EnsembleAction.fromYaml(payload['onComplete']),
     );
-
-    return HapticAction(hapticType);
   }
 
   @override
   Future<dynamic> execute(BuildContext context, ScopeManager scopeManager) {
-    return switch (type) {
-      HapticType.heavyImpact => HapticFeedback.heavyImpact(),
-      HapticType.mediumImpact => HapticFeedback.mediumImpact(),
-      HapticType.lightImpact => HapticFeedback.lightImpact(),
-      HapticType.selectionClick => HapticFeedback.selectionClick(),
-      HapticType.vibrate => HapticFeedback.vibrate(),
-    };
+    final evaluatedString = scopeManager.dataContext.eval(type);
+    final hapticType = HapticType //
+        .values
+        .firstWhere(
+      (e) => e.name == evaluatedString,
+      orElse: () => HapticType.heavyImpact,
+    );
+
+    switch (hapticType) {
+      case HapticType.heavyImpact:
+        HapticFeedback.heavyImpact();
+      case HapticType.mediumImpact:
+        HapticFeedback.mediumImpact();
+      case HapticType.lightImpact:
+        HapticFeedback.lightImpact();
+      case HapticType.selectionClick:
+        HapticFeedback.selectionClick();
+      case HapticType.vibrate:
+        HapticFeedback.vibrate();
+    }
+
+    if (onComplete != null) {
+      ScreenController().executeActionWithScope(
+        context,
+        scopeManager,
+        onComplete!,
+      );
+    }
+
+    return Future.value(null);
   }
 }
