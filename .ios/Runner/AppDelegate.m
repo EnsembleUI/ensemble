0d4d7df3071c858d9ad6b531e7ce88ec0a5d6ea8
Hemish Pancholi <hemishpancholi11@gmail.com>
2023-11-13 22:24:32 +0530

    Removed unwanted changes

--- .ios/Runner/AppDelegate.m
+++ .ios/Runner/AppDelegate.m
@@ -1,10 +1,12 @@
 #import "AppDelegate.h"
 #import "FlutterPluginRegistrant/GeneratedPluginRegistrant.h"
+#import "GoogleMaps/GoogleMaps.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+	[GMSServices provideAPIKey:@"AIzaSyD8vwvoaEPEgYemp1EkIETetJMvyS4Ptqk"];
   [GeneratedPluginRegistrant registerWithRegistry:self];
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
