767127b467567a20cf966136cd3d417ebee65da1
vinothvino42 <vinothvino42@gmail.com>
2024-01-08 21:31:00 +0530

    refactor: change svg image checking logic

--- lib/widget/image.dart
+++ lib/widget/image.dart
@@ -281,8 +281,15 @@ class ImageState extends WidgetState<EnsembleImage> {
   bool isSvg() {
     // Bytes Image
     if (widget._controller.source is List<dynamic>) {
-      String bytesAsString = utf8.decode(widget._controller.source);
-      return bytesAsString.contains('<svg') || bytesAsString.contains('<xml');
+      final Uint8List imageBytes =
+          Uint8List.fromList(widget._controller.source);
+
+      return imageBytes.length >= 5 &&
+          imageBytes[0] == 0x3C &&
+          imageBytes[1] == 0x3F &&
+          imageBytes[2] == 0x78 &&
+          imageBytes[3] == 0x6D &&
+          imageBytes[4] == 0x6C;
     }
 
     // String path image
