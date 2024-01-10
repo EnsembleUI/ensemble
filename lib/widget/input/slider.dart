4376dd4bf8d66730960058815aaa91f6adf7b422
snehmehta <snehmehta933@gmail.com>
2024-01-10 16:52:10 +0530

    slider widget

--- lib/widget/input/slider.dart
+++ lib/widget/input/slider.dart
@@ -29,9 +29,7 @@ class EnsembleSlider extends StatefulWidget
 
   @override
   Map<String, Function> methods() {
-    return {
-      'getValue': () => _controller.value.round(),
-    };
+    return {};
   }
 
   @override
@@ -39,12 +37,26 @@ class EnsembleSlider extends StatefulWidget
     return {
       'initialValue': (value) =>
           _controller.value = Utils.optionalDouble(value) ?? 0,
-      'min': (value) => _controller.minValue = Utils.optionalDouble(value) ?? 0,
+      'min': (value) =>
+          _controller.minValue = Utils.getDouble(value, fallback: 0.0),
       'max': (value) =>
-          _controller.maxValue = Utils.optionalDouble(value) ?? 100,
+          _controller.maxValue = Utils.getDouble(value, fallback: 1.0),
       'onChange': (definition) => _controller.onChange =
           EnsembleAction.fromYaml(definition, initiator: this),
       'divisions': (value) => _controller.divisions = Utils.optionalInt(value),
+      'thumbColor': (value) => _controller.thumbColor = Utils.getColor(value),
+      'inactiveTrackColor': (value) =>
+          _controller.inactiveTrackColor = Utils.getColor(value),
+      'activeTrackColor': (value) =>
+          _controller.activeTrackColor = Utils.getColor(value),
+      'activeTickMarkColor': (value) =>
+          _controller.activeTickMarkColor = Utils.getColor(value),
+      'inactiveTickMarkColor': (value) =>
+          _controller.inactiveTickMarkColor = Utils.getColor(value),
+      'trackHeight': (value) =>
+          _controller.trackHeight = Utils.optionalDouble(value),
+      'thumbRadius': (value) =>
+          _controller.thumbRadius = Utils.getDouble(value, fallback: 10),
     };
   }
 }
@@ -52,214 +64,88 @@ class EnsembleSlider extends StatefulWidget
 class SliderController extends FormFieldController {
   double value = 0.0;
   double minValue = 0.0;
-  double maxValue = 100.0;
+  double maxValue = 1.0;
   int? divisions;
   EnsembleAction? onChange;
+
+  Color? thumbColor;
+  Color? inactiveTrackColor;
+  Color? activeTrackColor;
+  Color? activeTickMarkColor;
+  Color? inactiveTickMarkColor;
+
+  double? trackHeight;
+  double thumbRadius = 10;
+}
+
+int calculateDecimalPlaces(double min, double max, int? divisions) {
+  if (divisions == null) return 1;
+  double interval = (max - min) / divisions;
+
+  int decimalPlaces = 1;
+  while ((interval *= 10) < 1) {
+    decimalPlaces++;
+  }
+
+  return decimalPlaces;
 }
 
 class SliderState extends FormFieldWidgetState<EnsembleSlider> {
   @override
   Widget buildWidget(BuildContext context) {
+    int decimalPlaces = calculateDecimalPlaces(widget.controller.minValue,
+        widget.controller.maxValue, widget.controller.divisions);
+
     return InputWrapper(
       type: EnsembleSlider.type,
       controller: widget.controller,
       widget: FormField<double>(
         key: validatorKey,
         validator: (value) {
-          // You can add validation logic here.
+          if (widget._controller.required &&
+              widget.controller.value == widget.controller.minValue) {
+            return Utils.translateWithFallback(
+                'ensemble.input.required', 'This field is required');
+          }
           return null;
         },
         builder: (FormFieldState<double> field) {
-          return SliderWidget(
-            min: widget._controller.minValue,
-            max: widget._controller.maxValue,
-            value: widget._controller.value,
-            divisions: widget._controller.divisions,
-            onChanged: isEnabled()
-                ? (value) {
-                    setState(() {
-                      widget._controller.value = value;
-                    });
-                    if (widget._controller.onChange != null) {
-                      ScreenController().executeAction(
-                        context,
-                        widget._controller.onChange!,
-                        event: EnsembleEvent(widget),
-                      );
+          return SliderTheme(
+            data: SliderTheme.of(context).copyWith(
+              thumbColor: widget.controller.thumbColor,
+              inactiveTrackColor: widget.controller.inactiveTrackColor,
+              activeTrackColor: widget.controller.activeTrackColor,
+              activeTickMarkColor: widget.controller.activeTickMarkColor,
+              inactiveTickMarkColor: widget.controller.inactiveTickMarkColor,
+              thumbShape: RoundSliderThumbShape(
+                enabledThumbRadius: widget._controller.thumbRadius,
+              ),
+              trackHeight: widget.controller.trackHeight,
+            ),
+            child: Slider(
+              label: widget.controller.value.toStringAsFixed(decimalPlaces),
+              min: widget.controller.minValue,
+              max: widget.controller.maxValue,
+              value: widget.controller.value,
+              divisions: widget.controller.divisions,
+              onChanged: isEnabled()
+                  ? (value) {
+                      setState(() {
+                        widget.controller.value = value;
+                      });
+                      if (widget.controller.onChange != null) {
+                        ScreenController().executeAction(
+                          context,
+                          widget.controller.onChange!,
+                          event: EnsembleEvent(widget),
+                        );
+                      }
                     }
-                  }
-                : null,
-            fullWidth: true,
+                  : null,
+            ),
           );
         },
       ),
     );
-  }
-}
-
-class SliderWidget extends StatefulWidget {
-  final double min;
-  final double max;
-  final double value;
-  final int? divisions;
-  final ValueChanged<double>? onChanged;
-  final double sliderHeight;
-  final bool fullWidth;
-
-  const SliderWidget({
-    super.key,
-    required this.min,
-    required this.max,
-    required this.value,
-    this.onChanged,
-    this.divisions,
-    this.sliderHeight = 48,
-    required this.fullWidth,
-  });
-
-  @override
-  State<SliderWidget> createState() => _SliderWidgetState();
-}
-
-class _SliderWidgetState extends State<SliderWidget> {
-  @override
-  Widget build(BuildContext context) {
-    double paddingFactor = .2;
-
-    if (widget.fullWidth) paddingFactor = .3;
-
-    return SizedBox(
-      width: widget.fullWidth ? double.infinity : (widget.sliderHeight) * 5.5,
-      height: (widget.sliderHeight),
-      child: Padding(
-        padding: EdgeInsets.fromLTRB(widget.sliderHeight * paddingFactor, 2,
-            widget.sliderHeight * paddingFactor, 2),
-        child: Row(
-          children: <Widget>[
-            Text(
-              '${widget.min}',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: widget.sliderHeight * .3,
-                fontWeight: FontWeight.w700,
-                color: Colors.white,
-              ),
-            ),
-            SizedBox(
-              width: widget.sliderHeight * .1,
-            ),
-            Expanded(
-              child: Center(
-                child: SliderTheme(
-                  data: SliderTheme.of(context).copyWith(
-                    activeTrackColor: Colors.white.withOpacity(1),
-                    inactiveTrackColor: Colors.white.withOpacity(.5),
-                    trackHeight: 4.0,
-                    thumbShape: CustomSliderThumbRect(
-                      thumbRadius: widget.sliderHeight * .4,
-                      min: widget.min.toInt(),
-                      max: widget.max.toInt(),
-                      thumbHeight: 80,
-                    ),
-                    overlayColor: Colors.white.withOpacity(.4),
-                    activeTickMarkColor: Colors.white,
-                    inactiveTickMarkColor: Colors.red.withOpacity(.7),
-                  ),
-                  child: Slider(
-                    value: widget.value,
-                    min: widget.min,
-                    max: widget.max,
-                    onChanged: widget.onChanged,
-                    label: widget.value.round().toString(),
-                    divisions: widget.divisions,
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(
-              width: widget.sliderHeight * .1,
-            ),
-            Text(
-              '${widget.max}',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: widget.sliderHeight * .3,
-                fontWeight: FontWeight.w700,
-                color: Colors.white,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class CustomSliderThumbRect extends SliderComponentShape {
-  final double thumbRadius;
-  final double thumbHeight;
-  final int min;
-  final int max;
-
-  const CustomSliderThumbRect({
-    required this.thumbRadius,
-    required this.thumbHeight,
-    required this.min,
-    required this.max,
-  });
-
-  @override
-  Size getPreferredSize(bool isEnabled, bool isDiscrete) {
-    return Size.fromRadius(thumbRadius);
-  }
-
-  @override
-  void paint(
-    PaintingContext context,
-    Offset center, {
-    required Animation<double> activationAnimation,
-    required Animation<double> enableAnimation,
-    required bool isDiscrete,
-    required TextPainter labelPainter,
-    required RenderBox parentBox,
-    required SliderThemeData sliderTheme,
-    required TextDirection textDirection,
-    required double value,
-    required double textScaleFactor,
-    required Size sizeWithOverflow,
-  }) {
-    final Canvas canvas = context.canvas;
-
-    final rRect = RRect.fromRectAndRadius(
-      Rect.fromCenter(
-          center: center, width: thumbHeight * 1.2, height: thumbHeight * .6),
-      Radius.circular(thumbRadius * .4),
-    );
-
-    final paint = Paint()
-      ..color = sliderTheme.activeTrackColor!
-      ..style = PaintingStyle.fill;
-
-    TextSpan span = TextSpan(
-        style: TextStyle(
-            fontSize: thumbHeight * .3,
-            fontWeight: FontWeight.w700,
-            color: sliderTheme.thumbColor,
-            height: 1),
-        text: getValue(value));
-    TextPainter tp = TextPainter(
-        text: span,
-        textAlign: TextAlign.left,
-        textDirection: TextDirection.ltr);
-    tp.layout();
-    Offset textCenter =
-        Offset(center.dx - (tp.width / 2), center.dy - (tp.height / 2));
-
-    canvas.drawRRect(rRect, paint);
-    tp.paint(canvas, textCenter);
-  }
-
-  String getValue(double value) {
-    return (min + (max - min) * value).round().toString();
   }
 }
