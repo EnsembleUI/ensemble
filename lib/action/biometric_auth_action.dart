d0f953a9978669486d289c62b1013d0cb2367107
snehmehta <snehmehta933@gmail.com>
2024-04-23 17:58:59 +0530

    action: biometricAuth added

---
+++ lib/action/biometric_auth_action.dart
@@ -0,0 +1,99 @@
+// ignore_for_file: avoid_print, use_build_context_synchronously
+
+import 'package:ensemble/framework/action.dart';
+import 'package:ensemble/framework/scope.dart';
+import 'package:ensemble/screen_controller.dart';
+import 'package:ensemble/util/utils.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:local_auth/local_auth.dart';
+import 'package:local_auth/error_codes.dart' as auth_error;
+
+class BiometricAuthAction extends EnsembleAction {
+  EnsembleAction? onAuthenticated;
+  EnsembleAction? onAuthenticationFailed;
+  EnsembleAction? onSensorNotAvailable;
+  EnsembleAction? onSensorNotConfigured;
+  EnsembleAction? onError;
+  bool? allowConfiguration;
+  String? label;
+  String? androidTitle;
+
+  BiometricAuthAction({
+    this.onAuthenticated,
+    this.onAuthenticationFailed,
+    this.allowConfiguration,
+    this.androidTitle,
+    this.onError,
+    this.onSensorNotAvailable,
+    this.label,
+    this.onSensorNotConfigured,
+  });
+
+  @override
+  Future execute(BuildContext context, ScopeManager scopeManager) async {
+    try {
+      final LocalAuthentication auth = LocalAuthentication();
+
+      await auth.getAvailableBiometrics();
+      final bool canAuthenticateWithBiometrics = await auth.canCheckBiometrics;
+
+      final bool canAuthenticate = await auth.isDeviceSupported();
+      if (!(canAuthenticate || canAuthenticateWithBiometrics)) {
+        if (onSensorNotAvailable == null) return;
+        ScreenController().executeAction(context, onSensorNotAvailable!);
+        return;
+      }
+      final bool didAuthenticate = await auth.authenticate(
+        localizedReason: label ?? 'Please authenticate',
+        options: AuthenticationOptions(
+          biometricOnly: true,
+          useErrorDialogs: allowConfiguration ?? true,
+          stickyAuth: true,
+          sensitiveTransaction: true,
+        ),
+      );
+      if (didAuthenticate) {
+        if (onAuthenticated == null) return;
+        ScreenController().executeAction(context, onAuthenticated!);
+      } else {
+        if (onAuthenticationFailed == null) return;
+        ScreenController().executeAction(context, onAuthenticationFailed!);
+      }
+    } on PlatformException catch (e) {
+      if (e.code == "auth_in_progess") return;
+
+      if (e.code == auth_error.biometricOnlyNotSupported) {
+        if (onSensorNotAvailable == null) return;
+        ScreenController().executeAction(context, onSensorNotAvailable!);
+      } else if (e.code == auth_error.notAvailable) {
+        if (onSensorNotConfigured == null) return;
+        ScreenController().executeAction(context, onSensorNotConfigured!);
+      } else {
+        if (onError == null) return;
+        ScreenController().executeAction(context, onError!);
+      }
+    }
+    return;
+  }
+
+  static EnsembleAction? fromMap({Map? payload}) {
+    if (payload == null || payload['onAuthenticated'] == null) {
+      print("localAuth: onAuthenticated is required");
+      return null;
+    }
+    return BiometricAuthAction(
+      onAuthenticated: EnsembleAction.fromYaml(payload['onAuthenticated']),
+      onAuthenticationFailed:
+          EnsembleAction.fromYaml(payload['onAuthenticationFailed']),
+      onError: EnsembleAction.fromYaml(payload['onError']),
+      onSensorNotAvailable:
+          EnsembleAction.fromYaml(payload['onSensorNotAvailable']),
+      androidTitle: Utils.optionalString(payload['androidTitle']),
+      allowConfiguration: Utils.optionalBool(payload['allowConfiguration']),
+      label: Utils.optionalString(payload['label']),
+      onSensorNotConfigured:
+          EnsembleAction.fromYaml(payload['onSensorNotConfigured']),
+    );
+  }
+}
