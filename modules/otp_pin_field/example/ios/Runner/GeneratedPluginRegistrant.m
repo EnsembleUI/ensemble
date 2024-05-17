75e6c32af0ea914e24c565adf6eec4c264e27e5f
Hemish Pancholi <hemishpancholi11@gmail.com>
2024-05-18 01:05:15 +0530

    Resolved issues with some modules for monorepo

---
+++ modules/otp_pin_field/example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<otp_pin_field/OtpPinFieldPlugin.h>)
+#import <otp_pin_field/OtpPinFieldPlugin.h>
+#else
+@import otp_pin_field;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [OtpPinFieldPlugin registerWithRegistrar:[registry registrarForPlugin:@"OtpPinFieldPlugin"]];
+}
+
+@end
