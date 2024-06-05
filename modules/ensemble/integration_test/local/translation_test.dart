2f6c5e9149c532e98d29605535b692574c311ee5
Vu Le <vusters@gmail.com>
2024-06-04 22:31:22 -0700

    Fixed BottomNavBar trying to reload Views excessively, causing the scope to get lost

--- modules/ensemble/integration_test/local/translation_test.dart
+++ modules/ensemble/integration_test/local/translation_test.dart
@@ -1,7 +1,7 @@
 import 'package:ensemble/ensemble.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'framework/test_helper.dart';
+import '../framework/test_helper.dart';
 
 void main() {
   group('Default English locale', () {
@@ -14,20 +14,6 @@ void main() {
       await tester.tap(find.text("Show Toast"));
       await tester.pumpAndSettle();
       expect(find.text("Hello Hello Peter"), findsOneWidget);
-    });
-  });
-
-  group('Forcing Spanish locale', () {
-    testWidgets("forcing Spanish locale should work", (tester) async {
-      EnsembleConfig config =
-          await TestHelper.setupApp(appName: 'translation', forcedLocale: 'es');
-      await TestHelper.loadScreen(tester, 'Basic', config);
-
-      expect(find.text("Hola Peter"), findsOneWidget);
-
-      await tester.tap(find.text("Show Toast"));
-      await tester.pumpAndSettle();
-      expect(find.text("Hola Hola Peter"), findsOneWidget);
     });
   });
 }
