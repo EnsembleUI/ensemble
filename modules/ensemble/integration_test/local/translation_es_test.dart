479344988b7971520c396ce3853c3125537d51d1
Vu Le <vusters@gmail.com>
2024-06-21 12:51:10 -0700

    Emulate Javascript toDate/TimeString() with Date() object

--- modules/ensemble/integration_test/local/translation_es_test.dart
+++ modules/ensemble/integration_test/local/translation_es_test.dart
@@ -15,7 +15,23 @@ void main() {
       // test localized date/time/currency
       expect(find.text("20 jun 2024"), findsOneWidget);
       expect(find.text("14:02"), findsOneWidget);
-      expect(find.text("100,12€"), findsOneWidget);
+
+      // some locales add special "NBSP" character between tokens that should not be wrapped (such as PM or euro currency),
+      // so we need to account for the string match
+      const NBSP = " ";
+
+      // currency output uses "NBSP" instead of the space, so account for that.
+      String expectedCurrency = "100,12${NBSP}€";
+      expect(find.text(expectedCurrency), findsOneWidget);
+
+      // watch out for non-breaking space between P and M
+      String expectedDateStr = "jun 20 at 2:02p.${NBSP}m.";
+      expect(find.text(expectedDateStr), findsOneWidget);
+
+      // test using Date objects
+      expect(find.text("14/3/2022"), findsOneWidget);
+      expect(find.text("16:07:20"), findsOneWidget);
+      expect(find.text("14/3/2022, 16:07:20"), findsOneWidget);
 
       // test translations
       expect(find.text("Hola Peter"), findsOneWidget);
---
+++ modules/ensemble_ts_interpreter/test/test_utils.dart
@@ -0,0 +1,4 @@
+class TestUtils {
+  // matches non-breaking spaces   and
+  static const non_breaking_spaces_regex = "[\u00A0\u202F]";
+}
