9734c50fd961e39e72ea5737d8adf1d017ed7eba
Vu Le <vusters@gmail.com>
2024-08-19 17:04:09 -0700

    #1582 Fixed Custom Widget's onLoad not dispatching in some ocassion, plus itemTemplate optimization

--- modules/ensemble/integration_test/local/customWidget_test.dart
+++ modules/ensemble/integration_test/local/customWidget_test.dart
@@ -49,4 +49,26 @@ void main() {
       expect(tester.getSize(find.text("2x")).width, 200);
     });
   });
+
+  testWidgets("onLoad should be called for both new and re-used widget state", (tester) async {
+    await TestHelper.loadScreen(tester, 'Custom Widget - onLoad', config);
+
+    // after API called, widget's onLoad will change text to "hello"
+    await tester.pumpAndSettle(const Duration(seconds: 3));
+    expect(find.text('hello'), findsOneWidget);
+
+    // change the text to "Hi", then open and close bottom sheet, which will invoke the API again
+    await tester.tap(find.descendant(of: find.byType(Button), matching: find.text("Say Hi")));
+    await tester.pumpAndSettle();
+    expect(find.text('hi'), findsOneWidget);
+    await tester.tap(find.descendant(of: find.byType(Button), matching: find.text("Show Bottom Sheet")));
+    await tester.pumpAndSettle();
+    await tester.tap(find.descendant(of: find.byType(Button), matching: find.text("Close")));
+    await tester.pumpAndSettle();
+
+    // since modal's dismiss invoke the API, the Row should re-render. The child widget inside is re-created
+    // Regardless if the widget state is reused or not (it should re-use), onLoad should be called again, which
+    // is exactly what we are trying to confirm here since the text should be changed back to "hello"
+    expect(find.text('hello'), findsOneWidget);
+  });
 }
