41161ebd3b1e21d8bdddd468aefea5f31f2a7ffc
TheNoumanDev <m_nouman@hotmail.com>
2024-10-21 18:02:38 +0500

    remoded redundant code

--- modules/js_widget/lib/src/mobile/js_widget.dart
+++ modules/js_widget/lib/src/mobile/js_widget.dart
@@ -148,19 +148,7 @@ class JsWidgetState extends State<JsWidget> {
         fit: StackFit.expand,
         children: [
           !_isLoaded ? widget.loader : const SizedBox.shrink(),
-          widget.listener != null
-              ? WebViewWidget(
-                  controller: controller,
-                  gestureRecognizers: <Factory<OneSequenceGestureRecognizer>>{
-                    Factory<OneSequenceGestureRecognizer>(
-                      () => TapGestureRecognizer()
-                        ..onTapDown = (TapDownDetails details) {
-                          // No need to manually handle tap here; JavaScript will send messages via JsBridge
-                        },
-                    ),
-                  },
-                )
-              : WebViewWidget(controller: controller),
+          WebViewWidget(controller: controller),
         ],
       ),
     );
