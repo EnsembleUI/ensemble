03b398b7df17876fdc8302af8b34d0fca94ce35a
Yifeng Evan Shi <4132143+evshi@users.noreply.github.com>
2024-10-29 13:48:12 -0700

    Scope host cache by app id to avoid collisions (#1669)

--- modules/ensemble/lib/framework/definition_providers/host_cache_provider.dart
+++ modules/ensemble/lib/framework/definition_providers/host_cache_provider.dart
@@ -36,7 +36,7 @@ class HostCachedEnsembleProvider extends EnsembleDefinitionProvider {
     await hostCache.reload();
     String? theme;
     if (appModel.themeMapping != null &&
-        (theme ??= hostCache.getString(appModel.themeMapping!)) != null) {
+        (theme ??= hostCache.getString(_getCacheKey(appModel.themeMapping!))) != null) {
       return AppBundle(
           theme: loadYaml(theme!),
           resources: await appModel.getCombinedResources());
@@ -51,14 +51,14 @@ class HostCachedEnsembleProvider extends EnsembleDefinitionProvider {
     String? content;
 
     if (screenId != null) {
-      content = hostCache.getString(screenId);
+      content = hostCache.getString(_getCacheKey(screenId));
     } else if (screenName != null) {
       final screenId = appModel.screenNameMappings[screenName];
       if (screenId != null) {
-        content = hostCache.getString(screenId);
+        content = hostCache.getString(_getCacheKey(screenId));
       }
     } else if (appModel.homeMapping != null) {
-      content = hostCache.getString(appModel.homeMapping!);
+      content = hostCache.getString(_getCacheKey(appModel.homeMapping!));
     }
 
     return content != null
@@ -71,7 +71,11 @@ class HostCachedEnsembleProvider extends EnsembleDefinitionProvider {
       if (value == null || value is InvalidDefinition) {
         return;
       }
-      hostCache.setString(key, json.encode(value));
+      hostCache.setString(_getCacheKey(key), json.encode(value));
     });
+  }
+
+  _getCacheKey(String id) {
+    return '${appId}.${id}';
   }
 }
