80b9c8f54c753aca7884fd12ed449c5f019d981b
Your Name <you@example.com>
2024-12-17 19:38:30 +0530

    fix: auto complete dropdown selected label

--- modules/ensemble/lib/widget/input/dropdown.dart
+++ modules/ensemble/lib/widget/input/dropdown.dart
@@ -470,6 +470,8 @@ class SelectOneState extends FormFieldWidgetState<SelectOne>
                 if (kDebugMode) {
                   print('Selected: ${selection.value}');
                 }
+                widget.controller.textEditingController.text =
+                    selection.label ?? selection.value;
               },
               optionsViewBuilder: (BuildContext context,
                   AutocompleteOnSelected<SelectOneItem> onSelected,
