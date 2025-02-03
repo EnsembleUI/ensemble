7838ffd9abb617321da2ff8253b7208d1b8a35a1
mehsaandev <ehsaan2611@gmail.com>
2025-02-03 22:17:36 +0500

    Added itemTemplate

--- modules/ensemble/lib/widget/widget_registry.dart
+++ modules/ensemble/lib/widget/widget_registry.dart
@@ -43,7 +43,7 @@ import 'package:ensemble/widget/image_cropper.dart';
 import 'package:ensemble/widget/input/dropdown.dart';
 import 'package:ensemble/widget/input/form_date.dart';
 import 'package:ensemble/widget/input/form_textfield.dart';
-import 'package:ensemble/widget/input/taginput.dart';
+import 'package:ensemble/widget/input/taginput/taginput.dart';
 import 'package:ensemble/widget/input/form_time.dart';
 import 'package:ensemble/widget/input/slider/slider.dart';
 import 'package:ensemble/widget/loading_container.dart';
