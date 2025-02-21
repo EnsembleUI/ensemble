1f8df8997f7a9ab2dffae88ad96ac898cb49fa0a
Sharjeel Yunus <sharjeel924@gmail.com>
2025-02-21 20:29:41 +0500

    fix: ensure EnsembleConfigService is initialized before accessing config

--- modules/ensemble/lib/framework/ensemble_config_service.dart
+++ modules/ensemble/lib/framework/ensemble_config_service.dart
@@ -4,16 +4,26 @@ import 'package:yaml/yaml.dart';
 // EnsembleConfigService is a service that provides access to the ensemble-config.yaml file through static property once initialized
 class EnsembleConfigService {
   static YamlMap? _config;
+  static bool _isInitialized = false;
 
   static Future<void> initialize() async {
     final yamlString = await rootBundle.loadString('ensemble/ensemble-config.yaml');
     _config = loadYaml(yamlString);
+    _isInitialized = true;
   }
 
   static YamlMap get config {
     if (_config == null) {
-      throw StateError('EnsembleConfig not initialized');
+      // if config is not available, load the default config
+      _config = loadYaml('''
+definitions:
+  from: 'ensemble'
+      ''');
+      _isInitialized =
+          true;
     }
     return _config!;
   }
+
+  static bool get isInitialized => _isInitialized;
 }
