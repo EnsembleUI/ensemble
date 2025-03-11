1a90d86f67adaec24a15b769c85985b04bc6fdbd
mehsaandev <ehsaan2611@gmail.com>
2025-03-11 16:26:27 +0500

    reverted Button widget focus condition

--- modules/ensemble/lib/widget/button.dart
+++ modules/ensemble/lib/widget/button.dart
@@ -280,9 +280,7 @@ class ButtonState extends EWidgetState<Button> {
     // if focus in on a formfield (e.g. TextField), clicking on button will
     // not remove focus, so its value is never updated. Unfocus here before
     // executing button click ensure we get all the latest value of the form fields
-    if (widget._controller.submitForm == false) {
-      FocusManager.instance.primaryFocus?.unfocus();
-    }
+    FocusManager.instance.primaryFocus?.unfocus();
 
     // submit the form if specified
     if (widget._controller.submitForm == true) {
