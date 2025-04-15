093400678915e9cb5a1ebe2a90fa81db1347d626
thenoumandev <m_nouman@hotmail.com>
2025-04-15 15:47:24 +0500

    changed QR pacakge

--- modules/camera/lib/qr_code_scanner.dart
+++ modules/camera/lib/qr_code_scanner.dart
@@ -11,7 +11,7 @@ import 'package:ensemble/widget/helpers/box_wrapper.dart';
 import 'package:ensemble/widget/helpers/controllers.dart';
 import 'package:ensemble/widget/helpers/widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 
 class EnsembleQRCodeScannerImpl
     extends EnsembleWidget<EnsembleQRCodeScannerController>
@@ -45,11 +45,27 @@ class EnsembleQRCodeScannerController extends EnsembleBoxController {
   Color? overlayColor;
 
   List<BarcodeFormat> get allFormatsAllowed {
-    final allFormats = formatsAllowed
-        .map((format) => BarcodeFormat.values.from(format))
-        .whereType<BarcodeFormat>()
-        .toList();
-    return allFormats;
+    // Convert to mobile_scanner formats
+    final List<BarcodeFormat> formats = [];
+    for (var format in formatsAllowed) {
+      switch (format.toLowerCase()) {
+        case 'qrcode':
+          formats.add(BarcodeFormat.qrCode);
+          break;
+        case 'barcode':
+          formats.add(BarcodeFormat.code128);
+          formats.add(BarcodeFormat.code39);
+          formats.add(BarcodeFormat.code93);
+          formats.add(BarcodeFormat.codabar);
+          formats.add(BarcodeFormat.ean8);
+          formats.add(BarcodeFormat.ean13);
+          formats.add(BarcodeFormat.upcA);
+          formats.add(BarcodeFormat.upcE);
+          break;
+        // Add other formats as needed
+      }
+    }
+    return formats.isEmpty ? [BarcodeFormat.qrCode] : formats;
   }
 
   @override
@@ -110,123 +126,188 @@ class EnsembleQRCodeScannerState
     extends EnsembleWidgetState<EnsembleQRCodeScannerImpl>
     with QRCodeScannerAction {
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
-  QRViewController? qrController;
+  MobileScannerController? scannerController;
 
-  // In order to get hot reload to work we need to pause the camera if the platform
-  // is android, or resume the camera if the platform is iOS.
   @override
-  void reassemble() {
-    super.reassemble();
-    if (Platform.isAndroid) {
-      pauseCamera();
+  void initState() {
+    super.initState();
+    scannerController = MobileScannerController(
+      facing: widget.controller.initialCamera == CameraFacing.back
+          ? CameraFacing.back
+          : CameraFacing.front,
+      formats: widget.controller.allFormatsAllowed,
+    );
+
+    if (widget.controller.onInitialized != null) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        ScreenController().executeAction(
+          context,
+          widget.controller.onInitialized!,
+          event: EnsembleEvent(widget.controller),
+        );
+      });
     }
-    resumeCamera();
-  }
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-
-    widget.controller.qrCodeScannerAction = this;
-  }
-
-  @override
-  void didUpdateWidget(covariant EnsembleQRCodeScannerImpl oldWidget) {
-    super.didUpdateWidget(oldWidget);
 
     widget.controller.qrCodeScannerAction = this;
   }
 
   @override
   Widget buildWidget(BuildContext context) {
+    final cutOutHeight =
+        Utils.getDouble(widget.controller.cutOutHeight, fallback: 200);
+    final cutOutWidth =
+        Utils.getDouble(widget.controller.cutOutWidth, fallback: 200);
+
     return EnsembleBoxWrapper(
       boxController: widget.controller,
-      widget: QRView(
+      widget: MobileScanner(
         key: qrKey,
-        cameraFacing: widget.controller.initialCamera,
-        formatsAllowed: widget.controller.allFormatsAllowed,
-        overlayMargin: widget.controller.overlayMargin ?? EdgeInsets.zero,
-        overlay: QrScannerOverlayShape(
-          borderColor: widget.controller.cutOutBorderColor ?? Colors.red,
-          // borderColor: Colors.yellow,
-          overlayColor: widget.controller.overlayColor ??
-              const Color.fromRGBO(0, 0, 0, 80),
-          borderRadius: Utils.getDouble(widget.controller.cutOutBorderRadius,
-              fallback: 0),
-          borderLength: Utils.getDouble(widget.controller.cutOutBorderLength,
-              fallback: 40),
-          borderWidth: Utils.getDouble(widget.controller.cutOutBorderWidth,
-              fallback: 3.0),
-          cutOutHeight:
-              Utils.getDouble(widget.controller.cutOutHeight, fallback: 200),
-          cutOutWidth:
-              Utils.getDouble(widget.controller.cutOutWidth, fallback: 200),
+        controller: scannerController,
+        scanWindow: Rect.fromCenter(
+          center: MediaQuery.of(context).size.center(Offset.zero),
+          width: cutOutWidth,
+          height: cutOutHeight,
         ),
-        onQRViewCreated: _onQRViewCreated,
-        onPermissionSet: (ctrl, p) => _onPermissionSet(context, ctrl, p),
+        onDetect: (capture) {
+          if (capture.barcodes.isNotEmpty &&
+              widget.controller.onReceived != null) {
+            final barcode = capture.barcodes.first;
+            final data = {
+              'format': barcode.format.name,
+              'data': barcode.rawValue,
+              'rawBytes': barcode.rawBytes,
+            };
+
+            ScreenController().executeAction(
+              context,
+              widget.controller.onReceived!,
+              event: EnsembleEvent(widget.controller, data: data),
+            );
+          }
+        },
+        overlayBuilder: (context, constraints) {
+          return CustomPaint(
+            painter: QRScannerOverlayPainter(
+              borderColor: widget.controller.cutOutBorderColor ?? Colors.red,
+              borderRadius: Utils.getDouble(
+                  widget.controller.cutOutBorderRadius,
+                  fallback: 0),
+              cutOutSize: cutOutWidth,
+            ),
+            child: const SizedBox.expand(),
+          );
+        },
       ),
     );
   }
 
-  void _onQRViewCreated(QRViewController controller) {
-    setState(() {
-      qrController = controller;
-    });
-
-    if (widget.controller.onInitialized != null) {
-      ScreenController().executeAction(
-        context,
-        widget.controller.onInitialized!,
-        event: EnsembleEvent(widget.controller),
-      );
-    }
-
-    qrController?.scannedDataStream.listen((scanData) {
-      if (widget.controller.onReceived != null) {
-        final data = {
-          'format': scanData.format.name,
-          'data': scanData.code,
-          'rawBytes': scanData.rawBytes,
-        };
-
-        ScreenController().executeAction(
-          context,
-          widget.controller.onReceived!,
-          event: EnsembleEvent(widget.controller, data: data),
-        );
-      }
-    });
-  }
-
-  void _onPermissionSet(
-      BuildContext context, QRViewController ctrl, bool status) {
-    if (widget.controller.onPermissionSet != null) {
-      ScreenController().executeAction(
-        context,
-        widget.controller.onPermissionSet!,
-        event: EnsembleEvent(widget.controller, data: {'status': status}),
-      );
-    }
-  }
-
   @override
   void flipCamera() {
-    qrController?.flipCamera();
+    scannerController?.switchCamera();
   }
 
   @override
   void pauseCamera() {
-    qrController?.pauseCamera();
+    scannerController?.stop();
   }
 
   @override
   void resumeCamera() {
-    qrController?.resumeCamera();
+    scannerController?.start();
   }
 
   @override
   void toggleFlash() async {
-    await qrController?.toggleFlash();
+    await scannerController?.toggleTorch();
     widget.controller.isFlashOn = !widget.controller.isFlashOn;
   }
+
+  @override
+  void dispose() {
+    scannerController?.dispose();
+    super.dispose();
+  }
+}
+
+// Simplified overlay painter that just draws the border
+class QRScannerOverlayPainter extends CustomPainter {
+  final Color borderColor;
+  final double borderRadius;
+  final double cutOutSize;
+
+  QRScannerOverlayPainter({
+    required this.borderColor,
+    required this.borderRadius,
+    required this.cutOutSize,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    // Calculate the rectangle in the center
+    final rect = Rect.fromCenter(
+      center: size.center(Offset.zero),
+      width: cutOutSize,
+      height: cutOutSize,
+    );
+
+    // Draw the scanning rectangle border
+    final paint = Paint()
+      ..color = borderColor
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 3.0;
+
+    // Draw corners instead of full border
+    const cornerLength = 30.0;
+
+    // Top-left corner
+    canvas.drawLine(
+      rect.topLeft.translate(0, cornerLength),
+      rect.topLeft,
+      paint,
+    );
+    canvas.drawLine(
+      rect.topLeft,
+      rect.topLeft.translate(cornerLength, 0),
+      paint,
+    );
+
+    // Top-right corner
+    canvas.drawLine(
+      rect.topRight.translate(-cornerLength, 0),
+      rect.topRight,
+      paint,
+    );
+    canvas.drawLine(
+      rect.topRight,
+      rect.topRight.translate(0, cornerLength),
+      paint,
+    );
+
+    // Bottom-left corner
+    canvas.drawLine(
+      rect.bottomLeft.translate(0, -cornerLength),
+      rect.bottomLeft,
+      paint,
+    );
+    canvas.drawLine(
+      rect.bottomLeft,
+      rect.bottomLeft.translate(cornerLength, 0),
+      paint,
+    );
+
+    // Bottom-right corner
+    canvas.drawLine(
+      rect.bottomRight.translate(-cornerLength, 0),
+      rect.bottomRight,
+      paint,
+    );
+    canvas.drawLine(
+      rect.bottomRight,
+      rect.bottomRight.translate(0, -cornerLength),
+      paint,
+    );
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
 }
