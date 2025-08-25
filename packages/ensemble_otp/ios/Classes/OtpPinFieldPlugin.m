ca40cd99b4a370e84e90a2536d4a2233f69b0efb
Sharjeel Yunus <sharjeel924@gmail.com>
2025-08-26 02:27:31 +0500

    chore(release): update ensemble_otp to version 1.0.2
    - Fixed iOS build errors related to Swift header imports.
    - Corrected podspec configuration for proper Swift module generation.
    - Updated method channel naming for consistency.
    - Resolved podspec name mismatch issues.
    - Added proper Swift header configuration in podspec.

--- packages/ensemble_otp/ios/Classes/OtpPinFieldPlugin.m
+++ packages/ensemble_otp/ios/Classes/OtpPinFieldPlugin.m
@@ -1,11 +1,11 @@
 #import "OtpPinFieldPlugin.h"
-#if __has_include(<otp_pin_field/otp_pin_field-Swift.h>)
-#import <otp_pin_field/otp_pin_field-Swift.h>
+#if __has_include(<ensemble_otp/ensemble_otp-Swift.h>)
+#import <ensemble_otp/ensemble_otp-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "otp_pin_field-Swift.h"
+#import "ensemble_otp-Swift.h"
 #endif
 
 @implementation OtpPinFieldPlugin
