eb0254430a2443be2f51d9fc4634e1c53d3d8de5
Muhammad Usman <usmantech.me@gmail.com>
2026-09-12 01:07:27 +0500

    feat(tab-bar): add overlay indicator option

--- modules/ensemble/lib/layout/tab_bar.dart
+++ modules/ensemble/lib/layout/tab_bar.dart
@@ -100,6 +100,8 @@ abstract class BaseTabBar extends StatefulWidget
           _controller.dividerThickness = Utils.optionalInt(thickness, min: 0),
       'dividerPadding': (padding) =>
           _controller.dividerPadding = Utils.optionalInsets(padding),
+      'overlayIndicator': (overlay) =>
+          _controller.overlayIndicator = Utils.optionalBool(overlay),
       'selectedIndex': (index) =>
           _controller.selectedIndex = Utils.getInt(index, min: 0, fallback: 0),
       'onTabSelection': (action) => _controller.onTabSelection =
